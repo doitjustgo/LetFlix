69b3b9b85193f0955329a2b793c5ae7a2bce18e3
doitjustgo <201102498@cs-cnu.org>
2024-03-14 23:50:37 +0900

    test v

--- app/(home)/apiurl.ts
+++ app/(home)/apiurl.ts
@@ -1,4 +1,2 @@
-const key = process.env.MY_API_KEY;
-
 //export const API_URL = `https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&nation=%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD&NumOfRow=30&releaseDts=20100101&listCount=30&releaseDte=20231231&detail=y&use=%EA%B7%B9%EC%9E%A5%EC%9A%A9&actor=%EC%9C%A0%ED%95%B4%EC%A7%84&ServiceKey=${key}`;
 export const API_URL = `https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&nation=%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD&NumOfRow=30&releaseDts=20100101&listCount=30&releaseDte=20231231&detail=y&use=%EA%B7%B9%EC%9E%A5%EC%9A%A9&actor=%EC%9C%A0%ED%95%B4%EC%A7%84&ServiceKey=123`;
