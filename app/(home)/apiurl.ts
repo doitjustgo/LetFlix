648b5437a640ee9d31740a5b58b6b9cfab6b80bf
doitjustgo <201102498@cs-cnu.org>
2024-04-14 17:34:49 +0900

    240414

--- app/(home)/apiurl.ts
+++ app/(home)/apiurl.ts
@@ -1,14 +1,16 @@
 const key = process.env.NEXT_PUBLIC_API_KEY;
 
-export const API_URL = `https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ratedYn=y&releaseDts=20110101&listCount=30&releaseDte=20240331&use=극장용&ServiceKey=${key}`;
-//https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&nation=대한민국&ratedYn=y&releaseDts=20110101&listCount=10&releaseDte=20240331&use=극장용&ServiceKey=YXL359P74592YBN721WI
+export const API_URL = `https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&nation=대한민국&actor=유해진&ratedYn=y&releaseDts=20110101&listCount=30&releaseDte=20240331&use=극장용&ServiceKey=${key}`;
+
 export async function getMovies(title) {
   var response;
 
   if (!title || title === '') {
     response = await fetch(`${API_URL}`);
   } else {
-    response = await fetch(`${API_URL}&title=${title}`);
+    response = await fetch(
+      `https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&use=극장용&ServiceKey=${key}&title=${title}`
+    );
   }
 
   const json = await response.json();
